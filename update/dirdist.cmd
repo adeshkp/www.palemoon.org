a2f23f445ca177153a7bec542aecfca5a0f55cb7
wolfbeast <mcwerewolf@gmail.com>
2016-11-23 13:42:39 +0100

    Make atom update setup static.

--- update/dirdist.cmd
+++ update/dirdist.cmd
@@ -2,12 +2,10 @@
 echo Copy v25...
 for /D %%a in (25.*) do copy update-x86.xml %%a\WINNT_x86-msvc\update.xml >nul:
 for /D %%a in (25.*) do copy update-x64.xml %%a\WINNT_x86_64-msvc\update.xml >nul:
-for /D %%a in (25.*) do copy update-atom.xml %%a\Atom\update.xml >nul:
 
 echo Copy v26...
 for /D %%a in (26.*) do copy update-x86.xml %%a\WINNT_x86-msvc\update.xml >nul:
 for /D %%a in (26.*) do copy update-x64.xml %%a\WINNT_x86_64-msvc\update.xml >nul:
-for /D %%a in (26.*) do copy update-atom.xml %%a\Atom\update.xml >nul:
 
 echo Copy v27...
 for /D %%a in (27.*) do copy update-x86.xml %%a\WINNT_x86-msvc\update.xml >nul:
