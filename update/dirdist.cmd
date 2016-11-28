95b1b0096b2f6051b563dc6a83799eba2177dc78
wolfbeast <mcwerewolf@gmail.com>
2016-11-28 12:30:45 +0100

    keep transitional version update current

--- update/dirdist.cmd
+++ update/dirdist.cmd
@@ -7,6 +7,9 @@ echo Copy v26...
 for /D %%a in (26.*) do copy update-x86.xml %%a\WINNT_x86-msvc\update.xml >nul:
 for /D %%a in (26.*) do copy update-x64.xml %%a\WINNT_x86_64-msvc\update.xml >nul:
 
+echo 26.5.0 transition...
+copy update-x86.xml 26.5.0\Atom\update.xml >nul:
+
 echo Copy v27...
 for /D %%a in (27.*) do copy update-x86.xml %%a\WINNT_x86-msvc\update.xml >nul:
 for /D %%a in (27.*) do copy update-x64.xml %%a\WINNT_x86_64-msvc\update.xml >nul:
