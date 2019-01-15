5f5ae36869c705d846f5bad7605846aac721411f
wolfbeast <mcwerewolf@wolfbeast.com>
2019-01-15 18:12:24 +0100

    28.3.0 release

--- download.php
+++ download.php
@@ -99,7 +99,7 @@ function constructDownloadURL($version, $mirror, $bits, $type) {
 // abusive or leading to DoS, should be uncommented.
 // funcCheckUserAgent();
 
-$version = '28.2.2';
+$version = '28.3.0';
 
 $mirror = funcHTTPGetValue('mirror');
 $bits = funcHTTPGetValue('bits');
