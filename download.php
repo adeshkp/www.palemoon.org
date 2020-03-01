e734be48fc6bf7b9fc2989b2b0b041ec002f61a2
wolfbeast <mcwerewolf@wolfbeast.com>
2020-03-01 10:34:27 +0100

    Update for 28.8.4

--- download.php
+++ download.php
@@ -112,7 +112,7 @@ function constructDownloadURL($version, $mirror, $bits, $type) {
 // abusive or leading to DoS, should be uncommented.
 // funcCheckUserAgent();
 
-$version = '28.8.3';
+$version = '28.8.4';
 
 $mirror = funcHTTPGetValue('mirror');
 $bits = funcHTTPGetValue('bits');
